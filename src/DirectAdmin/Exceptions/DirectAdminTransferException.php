bd876505a4eabdec641c750719ea470d980be266
Nick <info@ruddy.nl>
2016-12-07 17:26:05 +0100

    DirectAdmin Transfer Exception.

---
+++ src/DirectAdmin/Exceptions/DirectAdminTransferException.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+ * CopyRight 2016 NHosting.
+ * 
+ * For the full copyright and license information, please view 
+ * the LICENSE file that was distributed with this source code.
+ */
+
+namespace NHosting\DirectAdmin\Exceptions;
+
+/**
+ * DirectAdminTransferException Class
+ * 
+ * @author N.J. Vlug <info@ruddy.nl>
+ */
+class DirectAdminTransferException extends DirectAdminException
+{
+    
+}
